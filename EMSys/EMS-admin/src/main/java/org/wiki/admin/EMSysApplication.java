d92ffd99120511f0539bc73249d7cbea6c1375d4
SleepyWiKi <prince9798@icloud.com>
2023-06-19 19:25:38 +0800

    maven依赖优化，pom文件结构修改优化

---
+++ EMSys/EMS-admin/src/main/java/org/wiki/admin/EMSysApplication.java
@@ -0,0 +1,30 @@
+package org.wiki.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Packagename: org.wiki.admin
+ * @Classname: EMSysApplication
+ * @Description: EMSysApplication
+ * @Author: WiKi
+ * @Date: 23/06/19 6:46 PM
+ */
+@SpringBootApplication
+public class EMSysApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EMSysApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  EMSys启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+    
+}
---
+++ EMSys/EMS-generator/src/main/java/org/wiki/emsgenerator/EmsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.wiki.emsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsGeneratorApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EmsGeneratorApplication.class, args);
+    }
+    
+}
