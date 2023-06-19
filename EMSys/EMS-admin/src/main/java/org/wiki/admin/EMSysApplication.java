fbc69cda82585ae4015b8d7a729696baedf9c009
SleepyWiKi <prince9798@icloud.com>
2023-06-19 19:26:36 +0800

    maven依赖优化，pom文件结构修改优化

--- EMSys/EMS-admin/src/main/java/org/wiki/admin/EMSysApplication.java
+++ EMSys/EMS-admin/src/main/java/org/wiki/admin/EMSysApplication.java
@@ -15,16 +15,7 @@ public class EMSysApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(EMSysApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  EMSys启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println("EMSys启动成功！");
     }
     
 }
